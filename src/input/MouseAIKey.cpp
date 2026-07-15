001dc82baf0f8b81473c9f7772389c7f4cab51ae
turtlelnc <turtlelnc@outlook.com>
2026-07-15 16:00:07 +0800

    实现AI鼠标按键监听框架

---
+++ src/input/MouseAIKey.cpp
@@ -0,0 +1,16 @@
+#include "MouseAIKey.h"
+#include <iostream>
+
+MouseAIKey::MouseAIKey()
+{
+}
+
+void MouseAIKey::listen()
+{
+    std::cout<<"Mouse AI key listener started"<<std::endl;
+
+    // TODO:
+    // RawInput
+    // HID Vendor Usage
+    // AS7 device mapping
+}
