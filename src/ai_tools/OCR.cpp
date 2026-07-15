540c25af44a609f9b8c2cdc3b0675a3867819883
turtlelnc <turtlelnc@outlook.com>
2026-07-15 16:05:51 +0800

    实现OCR模块框架

---
+++ src/ai_tools/OCR.cpp
@@ -0,0 +1,14 @@
+#include "OCR.h"
+
+OCR::OCR()
+{
+}
+
+std::string OCR::recognize(const std::string& image)
+{
+    // TODO:
+    // Windows OCR
+    // PaddleOCR
+    // Tesseract
+    return "OCR result: " + image;
+}
