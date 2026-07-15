82ab4ac60a6884dff00b736d91eac6a85dfe52ed
turtlelnc <turtlelnc@outlook.com>
2026-07-15 15:52:11 +0800

    添加JSON接口占位

---
+++ src/network/Json.h
@@ -0,0 +1,15 @@
+#ifndef TURTLE_JSON_H
+#define TURTLE_JSON_H
+
+#include <string>
+
+class Json
+{
+public:
+    static std::string escape(const std::string& input)
+    {
+        return input;
+    }
+};
+
+#endif
