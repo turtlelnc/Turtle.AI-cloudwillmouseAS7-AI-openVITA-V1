6f4e773dcaea42bef7fba8af577d718cca1e6a3b
turtlelnc <turtlelnc@outlook.com>
2026-07-15 16:06:21 +0800

    添加代码分析模块接口

---
+++ src/ai_tools/CodeAnalyzer.h
@@ -0,0 +1,12 @@
+#ifndef TURTLE_CODE_ANALYZER_H
+#define TURTLE_CODE_ANALYZER_H
+
+#include <string>
+
+class CodeAnalyzer
+{
+public:
+    std::string explain(const std::string& code);
+};
+
+#endif
