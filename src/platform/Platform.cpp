5902bb61157d3b6e91daa41cd693c38661f06958
turtlelnc <turtlelnc@outlook.com>
2026-07-15 16:46:40 +0800

    Phase15 跨平台层：实现平台识别

---
+++ src/platform/Platform.cpp
@@ -0,0 +1,14 @@
+#include "Platform.h"
+
+std::string Platform::name()
+{
+#ifdef _WIN32
+    return "Windows";
+#elif __linux__
+    return "Linux";
+#elif __APPLE__
+    return "macOS";
+#else
+    return "Unknown";
+#endif
+}
